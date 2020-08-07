9b16e66cfc81e858150e76325c5e801838e8bf90
fayeyeye <fayekievit@hotmail.com>
2020-08-07 11:27:27 +0200

    movement + colliders

---
+++ bpw2/Assets/collision_test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision_test : MonoBehaviour
+{
+
+    private GameObject player;
+    private float playerX;
+    private float playerY;
+    public float timerTime;
+    float timer; 
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        playerX = player.transform.position.x;
+        playerY = player.transform.position.y;
+        if (Input.GetKey("space") && timer <= 0) {
+
+            Debug.Log("x is" + playerX + "y is" + playerY);
+            timer = timerTime;
+        }
+    }
+}
