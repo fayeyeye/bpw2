9eaf5a383ebe2fd647c5945a7006c5cd91a501c5
fayeyeye <fayekievit@hotmail.com>
2020-08-09 13:37:32 +0200

    post processing + switch terug naar dag + UI tekst

--- bpw2/Assets/collision_test.cs
+++ bpw2/Assets/collision_test.cs
@@ -47,7 +47,11 @@ public class collision_test : MonoBehaviour
 
     public Animator kader; 
 
-    public Animator vallendeSter; 
+    public Animator vallendeSter;
+
+    public GameObject catText;
+    public GameObject bellText;
+    public GameObject boatText;
     //public GameObject night; 
 
     private float playerX;
@@ -79,11 +83,13 @@ public class collision_test : MonoBehaviour
         if ((playerX >= -1.1 && playerX <= 0.3) && (playerY >= 5.1 && playerY <= 5.4))
         {
             bovenaan = 1;
+            bellText.SetActive(true);
 
             if (bovenaan > 0 && Input.GetKey("e") && timer <= 0)
             {
 
                 zonAnimator.SetInteger("switchTriggered", 1);
+                zonAnimator.SetInteger("switch2Triggered", 0);
 
                 globalLightAnimator.SetInteger("switchTriggered", 1);
                 globalLightAnimator.SetInteger("switch2Triggered", 0);
@@ -118,39 +124,76 @@ public class collision_test : MonoBehaviour
                 spriteLight8.SetInteger("switchTriggered", 1);
                 spriteLight9.SetInteger("switchTriggered", 1);
 
+                spriteLight1.SetInteger("switch2Triggered", 0);
+                spriteLight2.SetInteger("switch2Triggered", 0);
+                spriteLight3.SetInteger("switch2Triggered", 0);
+                spriteLight4.SetInteger("switch2Triggered", 0);
+                spriteLight5.SetInteger("switch2Triggered", 0);
+                spriteLight6.SetInteger("switch2Triggered", 0);
+                spriteLight7.SetInteger("switch2Triggered", 0);
+                spriteLight8.SetInteger("switch2Triggered", 0);
+                spriteLight9.SetInteger("switch2Triggered", 0);
 
                 reflected1.SetInteger("switchTriggered", 1);
                 reflected2.SetInteger("switchTriggered", 1);
                 reflected3.SetInteger("switchTriggered", 1);
                 reflected4.SetInteger("switchTriggered", 1);
 
+                reflected1.SetInteger("switch2Triggered", 0);
+                reflected2.SetInteger("switch2Triggered", 0);
+                reflected3.SetInteger("switch2Triggered", 0);
+                reflected4.SetInteger("switch2Triggered", 0);
+
                 reflected5.SetInteger("switchTriggered", 1);
                 reflected6.SetInteger("switchTriggered", 1);
+
+                reflected5.SetInteger("switch2Triggered", 0);
+                reflected6.SetInteger("switch2Triggered", 0);
 
                 pointLight1.SetInteger("switchTriggered", 1);
                 pointLight2.SetInteger("switchTriggered", 1);
                 pointLight3.SetInteger("switchTriggered", 1);
                 pointLight4.SetInteger("switchTriggered", 1);
 
+                pointLight1.SetInteger("switch2Triggered", 0);
+                pointLight2.SetInteger("switch2Triggered", 0);
+                pointLight3.SetInteger("switch2Triggered", 0);
+                pointLight4.SetInteger("switch2Triggered", 0);
+
                 kader.SetInteger("switchTriggered", 1);
+                kader.SetInteger("switch2Triggered", 0);
 
                 Invoke("VallendeSter", 2);
+                vallendeSter.SetInteger("dag", 0);
 
                 timer = timerTime;
                 alBovenGeweest = 1;
             }
         }
+
+        else
+        {
+            bellText.SetActive(false);
+        }
+
         // terugschakeling dag -> speler bij kat
         if ((playerX >= 1.3 && playerX <= 1.8) && (playerY >= -1.7 && playerY <= -1.2))
         {
             onderaan = 1;
+            catText.SetActive(true);
+
+
             if (onderaan > 0 && Input.GetKey("e") && timer <= 0)
             {
                 player.GetComponent<AudioSource>().enabled = true;
                 Invoke("miauw", 2);
                 Debug.Log("onderaan");
+
                 globalLightAnimator.SetInteger("switch2Triggered", 1);
                 globalLightAnimator.SetInteger("switchTriggered", 0);
+
+                zonAnimator.SetInteger("switchTriggered", 0);
+                zonAnimator.SetInteger("switch2Triggered", 1);
 
                 windowLights1.SetInteger("switch2Triggered", 1);
                 windowLights2.SetInteger("switch2Triggered", 1);
@@ -171,14 +214,82 @@ public class collision_test : MonoBehaviour
                 windowLights7.SetInteger("switchTriggered", 0);
                 windowLights8.SetInteger("switchTriggered", 0);
                 windowLights9.SetInteger("switchTriggered", 0);
+
+                spriteLight1.SetInteger("switch2Triggered", 1);
+                spriteLight2.SetInteger("switch2Triggered", 1);
+                spriteLight3.SetInteger("switch2Triggered", 1);
+                spriteLight4.SetInteger("switch2Triggered", 1);
+                spriteLight5.SetInteger("switch2Triggered", 1);
+                spriteLight6.SetInteger("switch2Triggered", 1);
+                spriteLight7.SetInteger("switch2Triggered", 1);
+                spriteLight8.SetInteger("switch2Triggered", 1);
+                spriteLight9.SetInteger("switch2Triggered", 1);
+
+                spriteLight1.SetInteger("switchTriggered", 0);
+                spriteLight2.SetInteger("switchTriggered", 0);
+                spriteLight3.SetInteger("switchTriggered", 0);
+                spriteLight4.SetInteger("switchTriggered", 0);
+                spriteLight5.SetInteger("switchTriggered", 0);
+                spriteLight6.SetInteger("switchTriggered", 0);
+                spriteLight7.SetInteger("switchTriggered", 0);
+                spriteLight8.SetInteger("switchTriggered", 0);
+                spriteLight9.SetInteger("switchTriggered", 0);
+
+                reflected1.SetInteger("switchTriggered", 0);
+                reflected2.SetInteger("switchTriggered", 0);
+                reflected3.SetInteger("switchTriggered", 0);
+                reflected4.SetInteger("switchTriggered", 0);
+
+                reflected1.SetInteger("switch2Triggered", 1);
+                reflected2.SetInteger("switch2Triggered", 1);
+                reflected3.SetInteger("switch2Triggered", 1);
+                reflected4.SetInteger("switch2Triggered", 1);
+
+                reflected5.SetInteger("switchTriggered", 0);
+                reflected6.SetInteger("switchTriggered", 0);
+
+                reflected5.SetInteger("switch2Triggered", 1);
+                reflected6.SetInteger("switch2Triggered", 1);
+
+                pointLight1.SetInteger("switchTriggered", 0);
+                pointLight2.SetInteger("switchTriggered", 0);
+                pointLight3.SetInteger("switchTriggered", 0);
+                pointLight4.SetInteger("switchTriggered", 0);
+
+                pointLight1.SetInteger("switch2Triggered", 1);
+                pointLight2.SetInteger("switch2Triggered", 1);
+                pointLight3.SetInteger("switch2Triggered", 1);
+                pointLight4.SetInteger("switch2Triggered", 1);
+
+                kader.SetInteger("switchTriggered", 0);
+                kader.SetInteger("switch2Triggered", 2);
+
+                vallendeSter.SetInteger("nacht", 0);
+                vallendeSter.SetInteger("dag", 1);
+
             }
         }
 
-        if ((playerX >= 0.4 && playerX <= 1.1) && (playerY >= -4.7 && playerY <= -4.0) && alBovenGeweest > 0 && Input.GetKey("e"))
+        else
         {
-            zonAnimator.SetInteger("switchTriggered", 1);
-            globalLightAnimator.SetInteger("GameEnd", 1);
-            Invoke("gameEnd", 2);
+            catText.SetActive(false);
+        }
+
+        if ((playerX >= 0.4 && playerX <= 1.1) && (playerY >= -4.7 && playerY <= -4.0 && alBovenGeweest > 0))
+        {
+            boatText.SetActive(true);
+
+            if (Input.GetKey("e"))
+            {
+                zonAnimator.SetInteger("switchTriggered", 1);
+                globalLightAnimator.SetInteger("GameEnd", 1);
+                Invoke("gameEnd", 2);
+            }
+        }
+
+        else
+        {
+            boatText.SetActive(false);
         }
     }
 
